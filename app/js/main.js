54902040a6f7d0c8d2db46369cd607cb96601fcf
Nikita Exzo <exzo73@gmail.com>
2021-12-02 22:03:38 +0200

    final version

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
-$(function () {
+var menu = document.querySelector('.header__wrapper');
+document.onscroll = function() {
 
-})
+  if (window.scrollY > 0 && !menu.classList.contains('fixed')) {
+    menu.classList.add('fixed');
+  }
+
+  if (window.scrollY <= 0 && menu.classList.contains('fixed')) {
+    menu.classList.remove('fixed');
+  }
+  
+};
